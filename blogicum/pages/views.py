0eb41eddfdec1ac8b651fe642062ac16c99c6546
Khamzaev <khamzaew.j@gmail.com>
2024-11-28 18:24:56 +0300

    new

---
+++ blogicum/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Страница "О нас"
+def about(request):
+    return render(request, 'pages/about.html')
+
+# Страница "Правила"
+def rules(request):
+    return render(request, 'pages/rules.html')
