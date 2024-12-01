29cd10f3e64fde0923280bccefdf6c401e7e973a
Khamzaev <khamzaew.j@gmail.com>
2024-12-01 19:53:24 +0300

    ready

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -3,14 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-7391l#@o5owl)@lxg@n(q6^8p8zn4gqbbl3k$fv@ot_jme3rpx'
-
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,14 +50,12 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = 'blogicum.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -78,7 +73,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
@@ -89,11 +83,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
